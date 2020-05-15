b950126e622a69c1f3bc0be297764b2de6d33f86
Bruno Henrique Ramos da Silva <brunohenrique.ramos@live.com>
2020-05-15 20:11:16 -0300

    calendar na rota home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 //import Sidebar from "../../components/Sidebar";
 import NavbarComponent from "../../components/Navbar";
 import React from "react";
+import Calendar from "../Calendar";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <>
         <NavbarComponent />
+        <Calendar />
       </>
     );
   }
