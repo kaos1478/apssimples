7b4ebbacd7b01172dce6130e68449029575fdd99
Bruno Henrique Ramos da Silva <brunohenrique.ramos@live.com>
2020-05-15 20:19:07 -0300

    voltando ao normal

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,12 @@
 //import Sidebar from "../../components/Sidebar";
 import NavbarComponent from "../../components/Navbar";
 import React from "react";
-import Calendar from "../Calendar";
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <>
         <NavbarComponent />
-        <Calendar />
       </>
     );
   }
